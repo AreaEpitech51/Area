5bf06e1d5a95da9d075bdda99cf674c38fef0096
florian <florian.azema@epitech.eu>
2023-12-13 17:01:20 +0100

    add front

--- area-web/app/page.tsx
+++ area-web/app/page.tsx
@@ -7,15 +7,29 @@ const Page = async () => {
   const session = await getPageSession();
   if (!session) redirect("/login");
   return (
-    <>
+    <body style={styles.page}>
       <h1>Profile</h1>
       <p>User id: {session.user.userId}</p>
       <p>Username: {session.user.username}</p>
       <Form action="/api/logout">
         <input type="submit" value="Sign out" />
       </Form>
-    </>
+    </body>
   );
+};
+
+const styles = {
+  page: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    margin: 0,
+    padding: 0,
+    background: "linear-gradient(to bottom, #222222, #000000)",
+    color: "#fff",
+    flexDirection: "column" as const,
+  },
 };
 
 export default Page;
