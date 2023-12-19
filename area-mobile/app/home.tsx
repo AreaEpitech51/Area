00a61fa5db08536e2c93c4e7866883c9235381fa
Julien Vignau-Espine <julien.vignau-espine@epitech.eu>
2023-12-19 21:08:42 +0100

    :feat: adding grid

--- area-mobile/app/home.tsx
+++ area-mobile/app/home.tsx
@@ -1,70 +1,88 @@
-import { Link } from "expo-router";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import React from "react";
+import { ScrollView, View, Text, StyleSheet, Image } from "react-native";
 
 const Home = () => {
-    return (
-        <View>
-            <Text>HOME</Text>
+  const renderGrid = () => {
+    const rows = 6;
+    const cols = 2;
+    const grid = [];
+    const icons_list = ["nasalogo", "google", "youtube"];
+    const defaultIcon = "question";
+    let case_number = 0;
+
+    const images = {
+      question: require('../assets/question.png'),
+      nasalogo: require('../assets/nasalogo.png'),
+      google: require('../assets/google.png'),
+      youtube: require('../assets/youtube.png')
+    };
+
+    for (let i = 0; i < rows; i++) {
+      const rowItems = [];
+      for (let j = 0; j < cols; j++) {
+        const iconName = case_number < icons_list.length
+          ? icons_list[case_number]
+          : defaultIcon;
+
+        rowItems.push(
+          <View key={j} style={styles.col}>
+            <Image
+              source={images[iconName]}
+              style={styles.image}
+            />
+          </View>
+        );
+        case_number += 1;
+      }
+      grid.push(
+        <View key={i} style={styles.row}>
+          {rowItems}
         </View>
-    );
-}
+      );
+    }
+
+    return grid;
+  };
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>Area</Text>
+      {renderGrid()}
+    </ScrollView>
+  );
+};
 
 export default Home;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#111",
-      alignItems: "center",
-      justifyContent: "center",
-      padding: 20,
-    },
-    mainBox: {
-        backgroundColor: "#222",
-        borderRadius: 10,
-        width: "100%",
-        paddingTop: 60,
-        paddingBottom: 60,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    title: {
-        fontSize: 30,
-        color: "#fff",
-        fontWeight: "bold",
-        marginBottom: "10%",
-    },
-    easy_log_box: {
-        backgroundColor: "#111",
-        width: "70%",
-        height: 50,
-        justifyContent: "center",
-        alignItems: "center",
-        margin: "1%",
-    },
-    input_zone: {
-        width: "80%",
-        margin: "1%",
-    },
-    textInput: {
-        borderColor: "#999",
-        borderWidth: 2,
-        borderRadius: 8,
-        height: 40,
-        paddingLeft: "5%",
-        color: "#FFF",
-        marginBottom: 20,
-    },
-    line: {
-        height: 0.8,
-        width: "90%",
-        margin: 30,
-        backgroundColor: "#999"
-    },
-    button: {
-        height: 60,
-        width: "60%",
-        backgroundColor: "#1abc54",
-    },
-  });
-  
+  container: {
+    flexGrow: 1,
+    justifyContent: "center",
+    backgroundColor: "#111",
+  },
+  row: {
+    flexDirection: "row",
+    marginVertical: 7.5,
+  },
+  col: {
+    flex: 1,
+    aspectRatio: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 15,
+    backgroundColor: "#222",
+    borderRadius: 20,
+  },
+  title: {
+    fontSize: 30,
+    color: "#fff",
+    fontWeight: "bold",
+    marginBottom: 20,
+    alignSelf: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+});
