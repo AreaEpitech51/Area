c8b3cfb552dce8d01602bea7634b19a966269a50
florian <florian.azema@epitech.eu>
2023-12-20 10:28:01 +0100

    trygettingtoken

--- area-web/app/api/token/google/route.ts
+++ area-web/app/api/token/google/route.ts
@@ -1,24 +1,43 @@
-import { googleAuth } from "@/auth/lucia";
-import { cookies, headers } from "next/headers";
-import type { NextRequest } from "next/server";
+import { OAuth2Client, Credentials } from 'google-auth-library';
+import http from 'http';
+import url from 'url';
+import open from 'open';
 
-export const GET = async (_request: NextRequest) => {
-  try {
-    const [url, state] = await googleAuth.getAuthorizationUrl();
-    cookies().set("google_oauth_state", state, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      path: "/",
-      maxAge: 60 * 60,
-    });
-    return new Response(null, {
-      status: 302,
-      headers: {
-        Location: url.toString(),
-      },
-    });
-  } catch (error) {
-    console.error(error);
-    return new Response(null, { status: 500 });
-  }
-};
+const client = new OAuth2Client(
+  'CLIENT_ID',
+  'CLIENT_SECRET',
+  'REDIRECT_URL'
+);
+
+async function getAccessToken(): Promise<void> {
+  const authenticatedClient = await getAuthenticatedClient();
+  const apiUrl = new URL('https://www.googleapis.com/oauth2/v4/token');
+
+  const res = await authenticatedClient.request({
+    url: apiUrl.href,
+    method: 'POST',
+    data: {
+      grant_type: 'authorization_code',
+      code: 'YOUR_AUTHORIZATION_CODE',
+      redirect_uri: 'REDIRECT_URL'
+    }
+  });
+  console.log(res.data);
+}
+
+async function getAuthenticatedClient(): Promise<OAuth2Client> {
+  const authUrl = client.generateAuthUrl({
+    access_type: 'offline',
+    scope: 'YOUR_REQUESTED_SCOPES'
+  });
+
+  await open(authUrl);
+
+  const authorizationCode = 'GET_THE_AUTHORIZATION_CODE_FROM_CALLBACK';
+  const { tokens } = await client.getToken(authorizationCode);
+  client.setCredentials(tokens);
+
+  return client;
+}
+
+getAccessToken().catch(console.error);
