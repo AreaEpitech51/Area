31abe17159a7bb2d3d1d58f72234d60699b4771e
florian <florian.azema@epitech.eu>
2024-01-08 18:06:11 +0100

    toggle

--- area-web/components/toggle.tsx
+++ area-web/components/toggle.tsx
@@ -12,9 +12,7 @@ const ToggleButton = () => {
     <div>
       <button
         onClick={toggleSwitch}
-        style={isOn ? { backgroundColor: "#4caf50", color: "#fff" } : { backgroundColor: "#d61c1c", color: "#000" }}
-        data-isOn={isOn}
-      >
+        style={isOn ? { backgroundColor: "#4caf50", color: "#fff" } : { backgroundColor: "#d61c1c", color: "#000" }}>
         {isOn ? "ON" : "OFF"}
       </button>
     </div>
